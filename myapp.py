6e68dcf77f3250577796c0e1d162c3cb9a39d70d
mofejo1 <eyinimofe98@gmail.com>
2025-09-15 14:11:25 +0100

    parameters reduced to tw, 1d para,eter removed

--- myapp.py
+++ myapp.py
@@ -16,7 +16,7 @@ tickerData = yf.Ticker(tickerSymbol)
 
 # Fetch historical stock data for the specified time period
 # period='1d' means daily data intervals
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(start='2010-5-31', end='2020-5-31')
 
 # Open   High   Low Close   Volume   Dividends   Stock Splits
 
